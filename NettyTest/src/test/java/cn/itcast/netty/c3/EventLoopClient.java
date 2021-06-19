51746fd261e844fef9174d4e074c3006b7b1fc60
mr-huangjian <mr.huangjian@foxmail.com>
2021-06-19 14:05:21 +0800

    Update EventLoopClient.java

--- NettyTest/src/test/java/cn/itcast/netty/c3/EventLoopClient.java
+++ NettyTest/src/test/java/cn/itcast/netty/c3/EventLoopClient.java
@@ -3,10 +3,7 @@ package cn.itcast.netty.c3;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -18,25 +15,22 @@ import java.nio.charset.Charset;
 @Slf4j
 public class EventLoopClient {
     public static void main(String[] args) throws InterruptedException {
-        Channel channel = new Bootstrap()
+        ChannelFuture channelFuture = new Bootstrap()
                 .group(new NioEventLoopGroup())
                 .channel(NioSocketChannel.class)
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         ch.pipeline().addLast(new StringEncoder());
-                        /*ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
-                            @Override
-                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                ByteBuf byteBuf = (ByteBuf) msg;
-                                log.debug(byteBuf.toString(Charset.defaultCharset()));
-                            }
-                        });*/
                     }
                 })
-                .connect("localhost", 8088)
-                .sync()
-                .channel();
+                .connect("localhost", 8088);
+
+        channelFuture.sync();
+
+        Channel channel = channelFuture.channel();
+        channel.writeAndFlush("hello, world");
+
         System.out.println(channel);
         System.out.println("");
     }
